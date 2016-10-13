dba98fe97d65f2de89adc0af2ac5d569723c0354
bethelmyd <bethelmyd@gmail.com>
2016-10-12 23:42:30 -0400

    star wars select current opponent

--- assets/js/starwars.js
+++ assets/js/starwars.js
@@ -1,16 +1,34 @@
+var wins = 0;
+var losses = 0;
+
 $(document).ready(function(){
-	$("#fighters div").on("click", function(event){
+	$("#fighters > div").on("click", function(event){
 		var opponentArea = $("#opponents");
-		var fighters = $("#fighters div");
+		var fighters = $("#fighters > div");
 		var clicked = this;
 		fighters.each(function(){
-			if(clicked !== this)
+			if(clicked !== this){
 				opponentArea.append(this);
+			}
+
 		});
 	});//end on click for fighters
 
 
+	$("#opponents > div").on("click", function(event){
+		var currentOpponentArea = $("#currentOpp");
+		console.log(currentOpponentArea);
+		currentOpponentArea.append(this);
+	});//end on click for opponents
 
+	$("#resetBtn").on("click", function(event){
+		reset();
+	});
+
+	$("#resetScoreBtn").on("click", function(event){
+		reset();
+		resetSummary();
+	});
 
 
 
@@ -18,3 +36,18 @@ $(document).ready(function(){
 
 
 });//end document ready
+
+function reset(){
+	var opponents = $("#opponents > div");
+	var fighterArea = $("#fighters");
+	fighterArea.append(opponents);
+
+}
+
+function resetSummary(){
+		wins = 0;
+		losses = 0;
+		$("#wins").html("0");	
+		$("#losses").html("0");	
+		$("#total").html("0");		
+}
